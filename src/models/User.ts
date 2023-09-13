05be847a26dba20729986885def5d8cbb15430f9
juanmiguel431 <juanmiguel431@gmail.com>
2023-09-12 23:17:59 -0400

    Salting and hashing the user passwords.

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
 
 const Schema = mongoose.Schema;
 
@@ -13,5 +14,44 @@ const userSchema = new Schema({
     required: true
   }
 });
+
+userSchema.pre('save', function (next, opts) {
+  const user = this;
+  if (!user.isModified('password')) {
+    next();
+  }
+
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, encrypted) => {
+      if (err) {
+        next(err);
+      }
+
+      user.password = encrypted;
+      next();
+    });
+  });
+});
+
+userSchema.methods.comparePassword = function (candidatePassword: string) {
+  const user = this;
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(candidatePassword, user.password, (err, same) => {
+      if (err) {
+        reject(err);
+      }
+
+      if (!same) {
+        reject(false);
+      }
+
+      resolve(true);
+    });
+  });
+};
 
 mongoose.model('User', userSchema);
